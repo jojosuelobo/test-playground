4b3fac333c1ce44ff167a808b05bd9450eb3c90c
Josue.Lobo <Josue.lobo@strategiced.com>
2026-09-11 16:15:42 -0300

    gui tests for sign up

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://playground.serverapp.com.br',
+    setupNodeEvents(on, config) {},
   },
 });
